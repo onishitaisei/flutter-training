ed3a076e5a53b91e3322495a3eefa3243eec4b05
onishi taisei <taisei.developr@gmail.com>
2022-01-04 18:17:50 +0900

    Animations in Flutterのチュートリアル終了

--- lib/flutter_tutorial_screen.dart
+++ lib/flutter_tutorial_screen.dart
@@ -67,6 +67,16 @@ class FlutterTutorialScreen extends StatelessWidget {
         appBar: AppBar(
           // タイトルテキスト
           title: const Text('Flutter layout demo'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.push<void>(
+                    context,
+                    MaterialPageRoute(builder: (context) => LogoApp()),
+                  );
+                },
+                icon: Icon(Icons.arrow_forward_ios))
+          ],
         ),
         body: ListView(
           children: [
@@ -154,6 +164,127 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
       ],
     );
   }
+}
 
-  // ···
+// class AnimatedLogo extends AnimatedWidget {
+//   const AnimatedLogo({Key? key, required Animation<double> animation})
+//       : super(key: key, listenable: animation);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final animation = listenable as Animation<double>;
+//     return Scaffold(
+//       appBar: AppBar(
+//         // タイトルテキスト
+//         title: const Text('Animation screen'),
+//       ),
+//       body: Center(
+//         child: Container(
+//           margin: const EdgeInsets.symmetric(vertical: 10),
+//           height: animation.value,
+//           width: animation.value,
+//           child: const FlutterLogo(),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class LogoApp extends StatefulWidget {
+//   const LogoApp({Key? key}) : super(key: key);
+
+//   @override
+//   _LogoAppState createState() => _LogoAppState();
+// }
+
+// class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+//   late Animation<double> animation;
+//   late AnimationController controller;
+
+//   @override
+//   void initState() {
+//     super.initState();
+//     controller =
+//         AnimationController(duration: const Duration(seconds: 2), vsync: this);
+//     animation = Tween<double>(begin: 0, end: 300).animate(controller)
+//       ..addStatusListener((status) {
+//         if (status == AnimationStatus.completed) {
+//           controller.reverse();
+//         } else if (status == AnimationStatus.dismissed) {
+//           controller.forward();
+//         }
+//       })
+//       ..addStatusListener((state) => print('$state'));
+
+//     controller.forward();
+//   }
+
+//   Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+//   @override
+//   void dispose() {
+//     controller.dispose();
+//     super.dispose();
+//   }
+// }
+
+class AnimatedLogo extends AnimatedWidget {
+  const AnimatedLogo({Key? key, required Animation<double> animation})
+      : super(key: key, listenable: animation);
+
+  // Make the Tweens static because they don't change.
+  static final _opacityTween = Tween<double>(begin: 0.1, end: 1);
+  static final _sizeTween = Tween<double>(begin: 0, end: 300);
+
+  @override
+  Widget build(BuildContext context) {
+    final animation = listenable as Animation<double>;
+    return Center(
+      child: Opacity(
+        opacity: _opacityTween.evaluate(animation),
+        child: Container(
+          margin: const EdgeInsets.symmetric(vertical: 10),
+          height: _sizeTween.evaluate(animation),
+          width: _sizeTween.evaluate(animation),
+          child: const FlutterLogo(),
+        ),
+      ),
+    );
+  }
+}
+
+class LogoApp extends StatefulWidget {
+  const LogoApp({Key? key}) : super(key: key);
+
+  @override
+  _LogoAppState createState() => _LogoAppState();
+}
+
+class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller =
+        AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = CurvedAnimation(parent: controller, curve: Curves.easeIn)
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.completed) {
+          controller.reverse();
+        } else if (status == AnimationStatus.dismissed) {
+          controller.forward();
+        }
+      });
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) => AnimatedLogo(animation: animation);
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
 }
