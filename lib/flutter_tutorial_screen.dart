39274c3ddd209e357455c1d3584a23bc589c1cf8
onishi taisei <taisei.onishi@axia-works.com>
2022-01-05 14:18:17 +0900

    不要なコメントアウトを削除

--- lib/flutter_tutorial_screen.dart
+++ lib/flutter_tutorial_screen.dart
@@ -175,67 +175,6 @@ class _FavoriteWidgetState extends State<FavoriteWidget> {
   }
 }
 
-// class AnimatedLogo extends AnimatedWidget {
-//   const AnimatedLogo({Key? key, required Animation<double> animation})
-//       : super(key: key, listenable: animation);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     final animation = listenable as Animation<double>;
-//     return Scaffold(
-//       appBar: AppBar(
-//         // タイトルテキスト
-//         title: const Text('Animation screen'),
-//       ),
-//       body: Center(
-//         child: Container(
-//           margin: const EdgeInsets.symmetric(vertical: 10),
-//           height: animation.value,
-//           width: animation.value,
-//           child: const FlutterLogo(),
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// class LogoApp extends StatefulWidget {
-//   const LogoApp({Key? key}) : super(key: key);
-
-//   @override
-//   _LogoAppState createState() => _LogoAppState();
-// }
-
-// class _LogoAppState extends State<LogoApp> with SingleTickerProviderStateMixin {
-//   late Animation<double> animation;
-//   late AnimationController controller;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     controller =
-//         AnimationController(duration: const Duration(seconds: 2), vsync: this);
-//     animation = Tween<double>(begin: 0, end: 300).animate(controller)
-//       ..addStatusListener((status) {
-//         if (status == AnimationStatus.completed) {
-//           controller.reverse();
-//         } else if (status == AnimationStatus.dismissed) {
-//           controller.forward();
-//         }
-//       })
-//       ..addStatusListener((state) => print('$state'));
-
-//     controller.forward();
-//   }
-
-//   Widget build(BuildContext context) => AnimatedLogo(animation: animation);
-//   @override
-//   void dispose() {
-//     controller.dispose();
-//     super.dispose();
-//   }
-// }
-
 class AnimatedLogo extends AnimatedWidget {
   const AnimatedLogo({Key? key, required Animation<double> animation})
       : super(key: key, listenable: animation);
