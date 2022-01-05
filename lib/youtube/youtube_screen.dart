c252b0fc53afbe646c7c72d0738a488c3175742a
onishi taisei <taisei.onishi@axia-works.com>
2022-01-05 18:58:22 +0900

    不要なコードを削除

--- lib/youtube/youtube_screen.dart
+++ lib/youtube/youtube_screen.dart
@@ -46,24 +46,6 @@ class YoutubeScreenPage extends StatelessWidget {
 
   List<int> dayPrevious = [1, 1];
 
-  int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
-  static const List<Widget> _widgetOptions = <Widget>[
-    Text(
-      'Index 0: Home',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 1: Business',
-      style: optionStyle,
-    ),
-    Text(
-      'Index 2: School',
-      style: optionStyle,
-    ),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
