91bc30a9cfd4a13da87f73d2ac18fd24c32b406d
onishi taisei <taisei.onishi@axia-works.com>
2022-01-06 16:48:49 +0900

    ヘッダー下のおすすめ部分を作成。

--- lib/residence/residence_screen.dart
+++ lib/residence/residence_screen.dart
@@ -9,6 +9,7 @@ class ResidenceScreen extends StatelessWidget {
   const ResidenceScreen({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size; // 画面サイズを取得
     return Scaffold(
       backgroundColor: _backgroundColor,
       appBar: AppBar(
@@ -87,14 +88,22 @@ class ResidenceScreen extends StatelessWidget {
         type: BottomNavigationBarType
             .fixed, // bottomnavigationが4つ以上の時は見えなくなってしまうため、type: BottomNavigationBarType.fixed,を追加
       ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 15,
+          ),
+          recommendation(size),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: _primaryColor,
         onPressed: () {},
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.search),
-            const Text(
+          children: const [
+            Icon(Icons.search),
+            Text(
               '物件',
               style: TextStyle(fontWeight: FontWeight.w900, fontSize: 10),
             ),
@@ -179,5 +188,160 @@ class ResidenceScreen extends StatelessWidget {
           backgroundColor: _primaryColor,
           foregroundColor: Colors.white,
         ));
+  }
+
+  Widget recommendation(Size size) {
+    return Center(
+      child: Container(
+        width: size.width * 0.97,
+        height: size.height * 0.15,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          color: _headerColor,
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.grey, //色
+              spreadRadius: 0.5,
+              blurRadius: 2,
+              offset: Offset(1, 1),
+            ),
+          ],
+        ),
+        child: Column(
+          children: [
+            recommendationHeader(),
+            recommendationContents(size),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget recommendationHeader() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 13.0,
+        right: 13.0,
+        left: 13.0,
+        bottom: 5.0,
+      ),
+      child: Row(
+        children: [
+          const Text(
+            'カウルのおすすめ',
+            style: TextStyle(
+              color: Color(0xff424242),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          const Text(
+            '新着3件',
+            style: TextStyle(
+              color: Color(0xffEA7961),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Spacer(),
+          Text(
+            '編集',
+            style: TextStyle(
+              color: _primaryColor,
+            ),
+          ),
+          const SizedBox(
+            width: 5,
+          ),
+          Icon(
+            Icons.edit,
+            color: _primaryColor,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget recommendationContents(size) {
+    return Container(
+      width: size.width * 0.93,
+      height: size.height * 0.09,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: _backgroundColor,
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.train,
+                  color: Color(0xff6A6A6A),
+                  size: 15,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  '東京駅・品川駅・川崎駅・横浜駅・目黒駅・恵比寿駅・渋谷駅・',
+                  style: TextStyle(
+                    color: Color(0xff6A6A6A),
+                    fontSize: 11,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.attach_money_sharp,
+                  color: Color(0xff6A6A6A),
+                  size: 15,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  '下限なし 〜 2,000万円',
+                  style: TextStyle(
+                    color: Color(0xff6A6A6A),
+                    fontSize: 11,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 2,
+            ),
+            Row(
+              children: [
+                Icon(
+                  Icons.info_outline,
+                  color: Color(0xff6A6A6A),
+                  size: 15,
+                ),
+                SizedBox(
+                  width: 5,
+                ),
+                Text(
+                  '1R 〜 4LDK / 10㎡以上 / 徒歩20分',
+                  style: TextStyle(
+                    color: Color(0xff6A6A6A),
+                    fontSize: 11,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
