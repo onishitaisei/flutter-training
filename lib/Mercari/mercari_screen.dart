7029cecee44b4cff5e43b84e4f1ac0ade0a5316d
onishi taisei <taisei.onishi@axia-works.com>
2022-01-07 13:32:07 +0900

    ボトムナビゲーションを作成。

--- lib/Mercari/mercari_screen.dart
+++ lib/Mercari/mercari_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class MercariScreen extends StatelessWidget {
-  const MercariScreen({Key? key}) : super(key: key);
+  MercariScreen({Key? key}) : super(key: key);
+
+  final _footerIconAndTextColor = const Color(0xff222222);
+  final double _footerIconSize = 30.0;
 
   @override
   Widget build(BuildContext context) {
@@ -12,11 +15,69 @@ class MercariScreen extends StatelessWidget {
         automaticallyImplyLeading: false, // デフォルトの戻るボタン(<)を削除
         title: const Text(
           '出品',
-          style: const TextStyle(
+          style: TextStyle(
             color: Color(0xff343434),
             fontWeight: FontWeight.w800,
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home_outlined,
+              color: _footerIconAndTextColor,
+              size: _footerIconSize,
+            ),
+            label: 'ホーム',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.notifications_outlined,
+              color: _footerIconAndTextColor,
+              size: _footerIconSize,
+            ),
+            label: 'お知らせ',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.camera_alt,
+              color: _footerIconAndTextColor,
+              size: _footerIconSize,
+            ),
+            label: '出品',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.qr_code,
+              color: _footerIconAndTextColor,
+              size: _footerIconSize,
+            ),
+            label: 'メルペイ',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outline,
+              color: _footerIconAndTextColor,
+              size: _footerIconSize,
+            ),
+            label: 'マイページ',
+          ),
+        ],
+
+        selectedItemColor: _footerIconAndTextColor,
+        unselectedItemColor: _footerIconAndTextColor,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        selectedLabelStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+        unselectedLabelStyle: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+        type: BottomNavigationBarType
+            .fixed, // bottomnavigationが4つ以上の時は見えなくなってしまうため、type: BottomNavigationBarType.fixed,を追加
       ),
       body: Container(),
     );
