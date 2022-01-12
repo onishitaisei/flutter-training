bff8fb8948e8e21d2c172da691146b8d5c7ddabb
onishi taisei <taisei.onishi@axia-works.com>
2022-01-12 18:02:06 +0900

    入力フォームにバリデーションを追加。

--- lib/async_await/async_screen.dart
+++ lib/async_await/async_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AsyncScreen extends StatefulWidget {
@@ -9,6 +10,7 @@ class AsyncScreen extends StatefulWidget {
 }
 
 class _AsyncScreen extends State<AsyncScreen> {
+  final _formKey = GlobalKey<FormState>();
   String name = '';
   String birthday = '';
   int age = -1;
@@ -33,15 +35,6 @@ class _AsyncScreen extends State<AsyncScreen> {
         age = prefs.getInt('age') ?? -1;
       },
     );
-  }
-
-  void delete() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      prefs.remove('name');
-      prefs.remove('birthday');
-      prefs.remove('age');
-    });
   }
 
   @override
@@ -72,10 +65,6 @@ class _AsyncScreen extends State<AsyncScreen> {
           Text(
             '誕生日 $birthday',
           ),
-          TextButton(
-            onPressed: () => delete(),
-            child: const Text('削除ボタン'),
-          ),
         ],
       ),
     );
@@ -98,36 +87,61 @@ class _AsyncScreen extends State<AsyncScreen> {
         return AlertDialog(
           title: const Text("登録"),
           content: SizedBox(
-            height: 180,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  onChanged: (value) {
-                    name = value;
-                  },
-                  decoration: const InputDecoration(
-                    labelText: "名前",
+            height: 270,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    autofocus: true, // FloatingActionButton押下後、自動的にフォーカス
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return '値を入力してください';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      name = value;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: "名前",
+                    ),
                   ),
-                ),
-                TextFormField(
-                  keyboardType: TextInputType.number,
-                  onChanged: (value) {
-                    age = int.parse(value);
-                  },
-                  decoration: const InputDecoration(
-                    labelText: "年齢",
+                  TextFormField(
+                    keyboardType: TextInputType.number, // ファーカス時に数値のキーボードを表示
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly
+                    ], // 数値の入力のみ受け付ける
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return '値を入力してください';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      age = int.parse(value);
+                    },
+                    decoration: const InputDecoration(
+                      labelText: "年齢",
+                    ),
                   ),
-                ),
-                TextFormField(
-                  onChanged: (value) {
-                    birthday = value;
-                  },
-                  decoration: const InputDecoration(
-                    labelText: "誕生日",
+                  TextFormField(
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return '値を入力してください';
+                      }
+                      return null;
+                    },
+                    onChanged: (value) {
+                      birthday = value;
+                    },
+                    decoration: const InputDecoration(
+                      labelText: "誕生日",
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
           actions: <Widget>[
@@ -139,8 +153,12 @@ class _AsyncScreen extends State<AsyncScreen> {
             ElevatedButton(
               child: const Text("保存"),
               onPressed: () {
-                _setValue();
-                Navigator.pop(context);
+                if (_formKey.currentState!.validate()) {
+                  // フォームに問題がなかったときの処理
+                  _setValue();
+                  Navigator.pop(context);
+                }
+                // Navigator.pop(context);
               },
             ),
           ],
