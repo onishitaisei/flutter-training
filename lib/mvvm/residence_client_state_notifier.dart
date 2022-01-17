2708ae1a8a552a28bc514fd21010698b41b934c8
onishi taisei <taisei.onishi@axia-works.com>
2022-01-17 18:11:30 +0900

    デプロイしたJSONファイルからデータを取得して動的にデータを表示できるように改修。

---
+++ lib/mvvm/residence_client_state_notifier.dart
@@ -0,0 +1,37 @@
+import 'package:training/mvvm/repository/residence_repository.dart';
+import 'package:training/mvvm/state/residence_client_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final residenceClientStateNotifier = StateNotifierProvider.autoDispose<
+    ResidenceClientStateNotifier,
+    ResidenceClientState>((ref) => ResidenceClientStateNotifier(ref.read));
+
+class ResidenceClientStateNotifier extends StateNotifier<ResidenceClientState> {
+  ResidenceClientStateNotifier(this._read)
+      : super(const ResidenceClientState()) {
+    fetchResidenceInformations();
+  }
+
+  final Reader _read;
+
+  Future<void> fetchResidenceInformations() async {
+    state = state.copyWith(isLoading: true);
+
+    final residenceInformations =
+        await _read(residenceRepositoryProvider).fetchResidenceInformations();
+
+    if (residenceInformations.isNotEmpty) {
+      state = state.copyWith(
+        isLoading: false,
+        isReadyData: true,
+        residenceInformations: residenceInformations,
+      );
+    } else {
+      state = state.copyWith(
+        isLoading: false,
+        isReadyData: false,
+        residenceInformations: residenceInformations,
+      );
+    }
+  }
+}
