508cd579a1e4785190938bdb5067b72be44c09ae
onishi taisei <taisei.developr@gmail.com>
2022-01-18 12:00:17 +0900

    不要なフィールドを削除、空の配列をを渡す部分を明示的に指定。

--- lib/mvvm/state/youtube_client_state.dart
+++ lib/mvvm/state/youtube_client_state.dart
@@ -7,7 +7,6 @@ part 'youtube_client_state.freezed.dart';
 class YoutubeClientState with _$YoutubeClientState {
   const factory YoutubeClientState({
     @Default(false) bool isLoading,
-    @Default(false) bool isReadyData,
     @Default(<YoutubeInformation>[])
         List<YoutubeInformation> youtubeInformation,
   }) = _YoutubeClientState;
