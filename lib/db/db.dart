5d1447f89a9e79578de80a388f5eb5245b8fca15
onishi taisei <taisei.developr@gmail.com>
2022-01-19 17:29:01 +0900

    Driftを使用するためのファイルの作成。

---
+++ lib/db/db.dart
@@ -0,0 +1,36 @@
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'package:drift/drift.dart';
+import 'dart:io';
+
+part 'db.g.dart';
+
+class Todos extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text().withLength(min: 6, max: 32)();
+  TextColumn get content => text().named('body')();
+  IntColumn get category => integer().nullable()();
+}
+
+@DataClassName("Category")
+class Categories extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get description => text()();
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
+
+@DriftDatabase(tables: [Todos, Categories])
+class MyDatabase extends _$MyDatabase {
+  MyDatabase() : super(_openConnection());
+  @override
+  int get schemaVersion => 1;
+}
